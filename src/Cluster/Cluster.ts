aa724626c0a242cab620c5eee7549ef30f935a6f
swlee <swlee@whatap.io>
2021-01-22 13:48:17 +0900

    Treemap

--- src/Cluster/Cluster.ts
+++ src/Cluster/Cluster.ts
@@ -1,4 +1,5 @@
 import { uid, DEFAULT_DATUM } from './ClusterMeta'
+import Treemap from './Treemap'
 
 const getRandomNumber = (y = 2) => Math.floor(Math.random() * Math.pow(10, y))
 const getRandomGroup = (prefix = 'group', y = 2) => prefix + Math.floor(Math.random() * Math.pow(10, y)).toString()
@@ -18,14 +19,8 @@ console.log('MOCK_DATA', MOCK_DATA)
 interface DynamicGroup {
   [groupNumber: string]: number | string
 }
-
-interface managedClusterDatum {
-  parentId?: string
-  children?: Cluster[]
-}
-
-interface StaticDatum extends managedClusterDatum {
-  value: string | number
+interface StaticDatum {
+  value: number
   name: string | number
 }
 
@@ -37,35 +32,46 @@ type ClusterManagedDatasource = {
 }
 
 interface ClusterProps {
-  depth: number
-  parentId?: string
+  depth?: number
+  parentId?: string | null
+  group?: string | number
+  index?: number
 }
+type Rect = [x: number, y: number, w: number, h: number]
 class Cluster {
-  private parentId: string | undefined
-  private id: string | undefined
+  private parentId: string | null
+  private id: string | null
   private depth: number = 0
+  private index: number = 0
+  private group: string | number = 'root'
+
   public isLeaf: boolean = false
   public datum: ClusterDatum | null = null
   public cluster: ClusterManagedDatasource = {}
-
-  constructor(props: ClusterProps = { depth: 0, parentId: 'root' }) {
-    const { depth = 0, parentId } = props
+  public values: (number | number[])[] = []
+  public node: (Rect | Rect[])[] = []
+  constructor(props: ClusterProps = {}) {
+    const { depth = 0, parentId = null, group = 'root', index = 0 } = props
     this.id = uid(depth)
     this.parentId = parentId
     this.depth = depth
+    this.group = group
+    this.index = index
   }
 
-  public draw = () => {}
-  public calculate = () => {}
   public setData = (datasource: ClusterData = []) => {
+    let thisDatum: ClusterDatum = { name: this.group, value: 0 }
     let datumGroupValueKey: keyof Partial<ClusterDatum> = `group${this.depth || ''}`
 
-    const managedDatasource = datasource.reduce<ClusterManagedDatasource>((acc, datum = DEFAULT_DATUM) => {
+    const managedDatasource = datasource.reduce<ClusterManagedDatasource>((acc, datum = DEFAULT_DATUM, index) => {
+      // acc this.datum
+      thisDatum.value += datum.value
+
+      // group value
       const groupValue = datum[datumGroupValueKey]
 
       // break
       if (!groupValue) {
-        this.datum = datum
         this.isLeaf = true
         return acc
       }
@@ -73,7 +79,8 @@ class Cluster {
       // push
       if (!acc[groupValue]) {
         acc[groupValue] = {
-          cluster: new Cluster({ depth: this.depth + 1, parentId: this.id }),
+          // new child cluster
+          cluster: new Cluster({ depth: this.depth + 1, parentId: this.id, group: groupValue, index }),
           datasource: [],
         }
       }
@@ -85,6 +92,42 @@ class Cluster {
       groupData.cluster.setData(groupData.datasource)
     })
     this.cluster = managedDatasource
+    this.datum = thisDatum
+  }
+
+  public flattenClusterData = (data: any[]) => {
+    Object.entries(this.cluster).forEach(([groupValue, groupData]) => {
+      if (groupData.cluster.isLeaf) {
+        data.push(groupData.datasource.map((datum) => datum))
+      } else {
+        let childData: Array<number | number[]> = []
+        groupData.cluster.flattenClusterData(childData)
+        data.push(childData)
+      }
+    })
+    return data
+  }
+
+  public calculate = ({ x = 0, y = 0, w, h }: { x: number; y: number; w: number; h: number }) => {
+    let flattenData: any[] = []
+    this.flattenClusterData(flattenData)
+    this.node = Treemap.generate(flattenData, w, h, x, y)
+    console.log('flattenData', flattenData)
+    console.log('this.node', this.node)
+  }
+  public draw = (render: (rect: any, datum: ClusterDatum) => any) => {
+    const callbackRender = (node: Rect | Rect[]) => {
+      if (Array.isArray(node)) {
+        if (typeof node[0] === 'number') {
+          render(node, { name: 'test', value: 0 })
+        } else {
+          node.forEach((child: any) => callbackRender(child))
+        }
+      }
+    }
+    this.node.forEach((rect) => {
+      callbackRender(rect)
+    })
   }
 
   public print = () => {
@@ -94,6 +137,9 @@ class Cluster {
 
 const c = new Cluster()
 c.setData(MOCK_DATA)
-c.print()
-
+c.calculate({ x: 0, y: 0, w: 100, h: 100 })
+c.draw(([x, y, w, h], datum) => {
+  console.log('render', [x, y, w, h], datum)
+})
+// c.print()
 export default Cluster
